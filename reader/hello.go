1caa7b142d83c9fe4605ba046d63cec5b81ada1b
igor <igorgonsalves96@hotmail.com>
2023-08-01 16:16:50 -0300

    feat: implementing a code to reate a log, fix: fixing some strings

--- reader/hello.go
+++ reader/hello.go
@@ -6,14 +6,17 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 )
 
 const monitoramentos = 3
-const delay = 5
+const delay = 10
+const dataFormat = "2/1/2006 15:04:05"
 
 func main() {
+
 	showIntroduction()
 
 	for {
@@ -35,14 +38,13 @@ func main() {
 }
 
 func showIntroduction() {
-	name := "Luffy"
-	fmt.Println("Hello sr ", name)
+	fmt.Println("Hello, sr. I'm happy to se you here")
 }
 
 func showMenu() {
 	fmt.Println("0- Exit")
 	fmt.Println("1- Start monitor")
-	fmt.Println("2- Show Logas")
+	fmt.Println("2- Show Logs")
 }
 
 func readCommand() int {
@@ -56,7 +58,7 @@ func starMonitor(sites []string) {
 	fmt.Println("Monitoring...")
 
 	for i, site := range sites {
-		fmt.Println("Testando site", i, ":", site)
+		fmt.Println("Testing", i, ":", site)
 		testaSite(site)
 
 		fmt.Println("")
@@ -79,9 +81,11 @@ func testaSite(site string) {
 	resp, _ := http.Get(site)
 
 	if resp.StatusCode == 200 {
-		fmt.Println("Site:", site, "foi carregado com sucesso!")
+		fmt.Println("Site:", site, "site has been loaded!")
+		logRegister(site, true)
 	} else {
-		fmt.Println("Site:", site, "está com problemas. Status Code:", resp.StatusCode)
+		fmt.Println("Site:", site, "Houston, we have a problem. Status Code:", resp.StatusCode)
+		logRegister(site, false)
 	}
 }
 
@@ -116,4 +120,16 @@ func readFile() []string {
 	fmt.Sprintln(sites)
 
 	return sites
+}
+
+func logRegister(site string, status bool) {
+	file, error := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+
+	if error != nil {
+		fmt.Println("Register log error")
+	}
+
+	file.WriteString(time.Now().Format(dataFormat) + " site: " + site + " " + strconv.FormatBool(status) + "\n")
+
+	file.Close()
 }
